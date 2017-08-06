e66ee34f45d66963c2bb8c2aa5523cc81d8124fb
Cheryl Kee <ksy.cheryl@gmail.com>
2017-08-06 20:59:04 +0800

    Format PollerRunner

--- monitor/src/main/java/exhibit/monitor/PollerRunner.java
+++ monitor/src/main/java/exhibit/monitor/PollerRunner.java
@@ -12,10 +12,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PollerRunner implements Runnable {
-	private static final String inputFolderDir = "D:/ExhibitMonitorData/Input";
-	private static final String processFolderDir = "D:/ExhibitMonitorData/Process";
-	private static final DateFormat inputDF = new SimpleDateFormat("dd/MM/yyyy");
-	private static final DateFormat lastModifiedDF = new SimpleDateFormat("HH:mm");
+	private static final String INPUT_FOLDER_DIR = "D:/ExhibitMonitorData/Input";
+	private static final String PROCESS_FOLDER_DIR = "D:/ExhibitMonitorData/Process";
+	private static final DateFormat INPUT_DF = new SimpleDateFormat("dd/MM/yyyy");
+	private static final DateFormat LAST_MODIFIED_DR = new SimpleDateFormat("HH:mm");
 	private Logger logger;
 
 	public PollerRunner() {
@@ -24,7 +24,7 @@ public class PollerRunner implements Runnable {
 
 	@Override
 	public void run() {
-		final File inputFolder = new File(inputFolderDir);
+		final File inputFolder = new File(INPUT_FOLDER_DIR);
 		while (true) {
 			for (final File fileEntry : inputFolder.listFiles()) {
 				if (fileEntry.isFile()) {
@@ -33,7 +33,7 @@ public class PollerRunner implements Runnable {
 						// Thread worker = new Thread(new WorkerRunner());
 						// worker.start();
 					} else {
-						File inputFile = new File("D:/ExhibitMonitorData/Input/" + fileEntry.getName());
+						File inputFile = new File(INPUT_FOLDER_DIR + "/" + fileEntry.getName());
 						inputFile.delete();
 					}
 				}
@@ -49,21 +49,21 @@ public class PollerRunner implements Runnable {
 	private boolean isDuplicate(File file) {
 		if (ApplicationContext.inputFileMap.isEmpty()) {
 			Calendar cal = Calendar.getInstance();
-			ApplicationContext.inputFileMap.put(file.getName(), inputDF.format(cal.getTime()));
+			ApplicationContext.inputFileMap.put(file.getName(), INPUT_DF.format(cal.getTime()));
 			return false;
 		}
 
 		Calendar cal = Calendar.getInstance();
 		if (ApplicationContext.inputFileMap.containsKey(file.getName())) {
-			return ApplicationContext.inputFileMap.get(file.getName()).equals(inputDF.format(cal.getTime()));
+			return ApplicationContext.inputFileMap.get(file.getName()).equals(INPUT_DF.format(cal.getTime()));
 		}
-		ApplicationContext.inputFileMap.put(file.getName(), inputDF.format(cal.getTime()));
+		ApplicationContext.inputFileMap.put(file.getName(), INPUT_DF.format(cal.getTime()));
 		return false;
 	}
 
 	private boolean isOnTime(File file) {
 		try {
-			String lastModifiedTimeStr = lastModifiedDF.format(file.lastModified());
+			String lastModifiedTimeStr = LAST_MODIFIED_DR.format(file.lastModified());
 			long lastModifiedTime = new SimpleDateFormat("HH:mm").parse(lastModifiedTimeStr).getTime();
 
 			Iterator<Entry<FileDetails, Map<String, String>>> iterator = ApplicationContext.xmlMap.entrySet()
@@ -88,8 +88,8 @@ public class PollerRunner implements Runnable {
 
 	private void moveFile(String fileName) {
 		try {
-			final File inputFile = new File(inputFolderDir + "/" + fileName);
-			inputFile.renameTo(new File(processFolderDir + "/" + inputFile.getName()));
+			final File inputFile = new File(INPUT_FOLDER_DIR + "/" + fileName);
+			inputFile.renameTo(new File(PROCESS_FOLDER_DIR + "/" + inputFile.getName()));
 
 		} catch (Exception e) {
 			logger.log(Level.FINEST, e.getMessage(), e);

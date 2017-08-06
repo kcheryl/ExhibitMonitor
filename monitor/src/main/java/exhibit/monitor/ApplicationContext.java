54ef41e64c4d7b004527dbddedf8260641cc14b9
Cheryl Kee <ksy.cheryl@gmail.com>
2017-08-06 20:36:20 +0800

    Attempt to fix vulnerability in ApplicationContext

--- monitor/src/main/java/exhibit/monitor/ApplicationContext.java
+++ monitor/src/main/java/exhibit/monitor/ApplicationContext.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ApplicationContext {
-	public static Map<FileDetails, Map<String, String>> xmlMap;
+	protected static Map<FileDetails, Map<String, String>> xmlMap;
 
 	static {
 		xmlMap = new HashMap<>();

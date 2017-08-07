e05f914d804d423e116411e89e9aa845923d6ee3
Cheryl Kee <ksy.cheryl@gmail.com>
2017-08-07 22:52:07 +0800

    Attempt to fix vulnerability in DBRunner

--- monitor/src/main/java/exhibit/monitor/InvalidDBRunner.java
+++ monitor/src/main/java/exhibit/monitor/InvalidDBRunner.java
@@ -23,9 +23,8 @@ public class InvalidDBRunner implements Runnable {
 			String sql = "CREATE TABLE INVALID_RECORDS " + "(id INTEGER not NULL AUTO_INCREMENT, "
 					+ " file_name VARCHAR(255), " + " date VARCHAR(255), " + " record_number INTEGER, "
 					+ " record VARCHAR(255), " + " PRIMARY KEY ( id ))";
-			stmt = conn.prepareStatement(sql);
-			stmt.execute();
-			stmt.closeOnCompletion();
+
+			queryStatement(conn, stmt, sql);
 
 			synchronized (ApplicationContext.invalidRecords) {
 				while (ApplicationContext.invalidRecords.isEmpty()) {
@@ -34,17 +33,7 @@ public class InvalidDBRunner implements Runnable {
 
 				for (Record record : ApplicationContext.invalidRecords) {
 					String query = "insert into invalid_records (file_name, date, record_number, record) values (?, ?, ?, ?)";
-					stmt = conn.prepareStatement(query);
-
-					stmt.setString(1, record.getFileName());
-					Calendar cal = Calendar.getInstance();
-					cal.setTime(record.getDate());
-					java.sql.Date sqlDate = new java.sql.Date(cal.getTime().getTime());
-					stmt.setDate(2, sqlDate);
-					stmt.setInt(3, record.getRecordNum());
-					stmt.setString(4, record.getRecord());
-					stmt.execute();
-					stmt.closeOnCompletion();
+					stmt = queryStatement(conn, stmt, record, query);
 				}
 			}
 		} catch (Exception e) {
@@ -52,24 +41,53 @@ public class InvalidDBRunner implements Runnable {
 			// e.printStackTrace();
 		} finally {
 			close(conn);
-			// close(stmt);
 		}
 
+	}
+
+	private PreparedStatement queryStatement(Connection conn, PreparedStatement stmt, Record record, String query) {
+		try {
+			stmt = conn.prepareStatement(query);
+			stmt.setString(1, record.getFileName());
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(record.getDate());
+			java.sql.Date sqlDate = new java.sql.Date(cal.getTime().getTime());
+			stmt.setDate(2, sqlDate);
+			stmt.setInt(3, record.getRecordNum());
+			stmt.setString(4, record.getRecord());
+			stmt.execute();
+		} catch (Exception e) {
+			// e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (Exception e) {
+				// e.printStackTrace();
+			}
+		}
+		return stmt;
+	}
+
+	private void queryStatement(Connection conn, PreparedStatement stmt, String sql) {
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.execute();
+		} catch (Exception e) {
+			// e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (Exception e) {
+				// e.printStackTrace();
+			}
+		}
 	}
 
 	private void close(Connection conn) {
-		if (conn == null) {
-			return;
-		}
-		try {
-			conn.close();
-		} catch (Exception e) {
-			logger.log(Level.FINEST, e.getMessage(), e);
-			// e.printStackTrace();
-		}
-	}
-
-	private void close(PreparedStatement conn) {
 		if (conn == null) {
 			return;
 		}
